6b3a05164e047a2f105e6f491a7c67004bea6f68
Atheris <accpcshd@gmail.com>
2024-07-14 17:43:12 +0330

    Fixed RenderingSystem not working when SubScene is closed

--- Assets/Scripts/SyncObjects/Directory.cs
+++ Assets/Scripts/SyncObjects/Directory.cs
@@ -5,21 +5,6 @@ namespace Snakey.SyncObjects
 {
     public class Directory : MonoBehaviour
     {
-        public static Directory Instance { get; private set; }
-
         public List<LineRenderer> LineRenderers;
-
-        private void Awake()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            else
-            {
-                Debug.LogError("Singleton already exists in the scene", gameObject);
-                Destroy(gameObject);
-            }
-        }
     }
 }
--- Assets/Scripts/Systems/SnakeRenderingSystem.cs
+++ Assets/Scripts/Systems/SnakeRenderingSystem.cs
@@ -1,41 +1,50 @@
 ﻿using Snakey.Components;
+using Snakey.SyncObjects;
 using Unity.Entities;
+using UnityEngine;
 using Utils;
 using Vector3 = UnityEngine.Vector3;
 
 namespace Snakey.Systems
 {
     [UpdateAfter(typeof(SnakeInitializationSystem))]
-    public partial struct SnakeRenderingSystem : ISystem
+    public partial class SnakeRenderingSystem : SystemBase
     {
-        public void OnCreate(ref SystemState state)
+        private Directory _directory;
+
+        protected override void OnCreate()
         {
-            state.RequireForUpdate<ManagedDirectory>();
+            RequireForUpdate<SnakeCoords>();
+        }
+        
+        protected override void OnStartRunning()
+        {
+            _directory = Object.FindAnyObjectByType<Directory>();
         }
 
-        public void OnUpdate(ref SystemState state)
+        protected override void OnUpdate()
         {
-            var managedDirectory = SystemAPI.ManagedAPI.GetSingleton<ManagedDirectory>();
-            var snakePositionBuffer = SystemAPI.GetSingletonBuffer<SnakeCoords>();
+
+            var snakeCoordsBuffer = SystemAPI.GetSingletonBuffer<SnakeCoords>();
             var bounds = SystemAPI.GetSingleton<GridBounds>().Bounds;
 
-            var lineRenderers = managedDirectory.LineRenderers;
+            var lineRenderers = _directory.LineRenderers;
             lineRenderers.ForEach(r => r.positionCount = 0);
 
             var rendererIndex = 0;
             var rendererPosIndex = 0;
-            var currentRenderer = managedDirectory.LineRenderers[rendererIndex];
-            var previousGridPos = snakePositionBuffer[0].Coords;
-            for (var i = 0; i < snakePositionBuffer.Length; i++)
+            var currentRenderer = _directory.LineRenderers[rendererIndex];
+            var previousGridPos = snakeCoordsBuffer[0].Coords;
+            for (var i = 0; i < snakeCoordsBuffer.Length; i++)
             {
-                var gridPos = snakePositionBuffer[i].Coords;
+                var gridPos = snakeCoordsBuffer[i].Coords;
                 var worldPos = GridUtils.GridCoordsToWorldPosition(gridPos, bounds);
                 if (!GridUtils.IsNextToOrTheSame(gridPos, previousGridPos))
                 {
-                    currentRenderer = managedDirectory.LineRenderers[++rendererIndex];
+                    currentRenderer = _directory.LineRenderers[++rendererIndex];
                     rendererPosIndex = 0;
                 }
-
+            
                 currentRenderer.positionCount++;
                 currentRenderer.SetPosition(rendererPosIndex++, new Vector3(worldPos.x, worldPos.y, 0));
                 previousGridPos = gridPos;
